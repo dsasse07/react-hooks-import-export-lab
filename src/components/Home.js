5bffe509d92a2b941f79b923d19d713f1ace34f5
Daniel Sasse <dsasse07@gmail.com>
2021-02-21 17:25:23 -0500

    Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
+import * as UserData from "../data/user"
+
+
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {UserData.username} is a Web Developer from {UserData.city}
       </h1>
     </div>
   );
 }
+
+export default Home
